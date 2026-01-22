ac63e660d218d8d221d7399690d70660666535a4
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-22 14:05:08 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import ListGroup from "./ListGroup";
 
 function App() {
    let items = ["Kigali", "Musanze", "Rubavu", "Huye", "Nyagatare"];
+
+   const handleSelectItem = (item: string) => {
+    console.log(item)
+   }
   return (
     <div className="App">
       <h1>List of Cities</h1>
-      <ListGroup items={items} heading="Cities"/>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
     </div>
   );
 }
