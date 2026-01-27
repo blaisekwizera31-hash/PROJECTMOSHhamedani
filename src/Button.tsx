ab0369031c88943811d4b113d635e660242bfd19
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-27 19:24:01 +0200

    commit

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
+interface Props {
+  children: string;
+}
 function Button() {
-  return (
-    <button className="btn btn-primary">Button</button>
-  );
+  return <button className="btn btn-primary">Button</button>;
 }
 
 export default Button;
