59f84c54bdbf15689a9135d2854876dcba15e5df
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-27 18:30:59 +0200

    commit

--- src/ListGroup.tsx
+++ src/ListGroup.tsx
@@ -1,7 +1,8 @@
-function App(){
-return 
-  <div>
-    
-  </div>
+import React from "react";
 
-}
+const Button = () => {
+  return 
+  <button className="btn btn-primary">Button</button>
+} 
+
+export default Button;
