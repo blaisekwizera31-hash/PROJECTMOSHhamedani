48c40b00405026a5df5db3a6478a1df4ea6c6ff9
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-28 15:35:25 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Form from "./Form";
+// import Form from "./Form";
 
 // function App() {
 //   return(
@@ -7,6 +7,47 @@ import Form from "./Form";
 //   </>
 //   )
 // }
-  
 
 // export default App;
+
+
+import ExpenseList from "./assets/expense-tracker/components/expenseList";
+
+const App = () => {
+  const expenses = [
+    {
+      id: 1,
+      description: "aaa",
+      amount: 100,
+      category: "Utilities",
+    },
+    {
+      id: 2,
+      description: "bbb",
+      amount: 200,
+      category: "Utilities",
+    },
+    {
+      id: 3,
+      description: "ccc",
+      amount: 300,
+      category: "Utilities",
+    },
+    {
+      id: 4,
+      description: "ddd",
+      amount: 400,
+      category: "Utilities",
+    },
+  ];
+  return (
+    <div>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => console.log("Deleted", id)}
+      />
+    </div>
+  );
+};
+
+export default App;
