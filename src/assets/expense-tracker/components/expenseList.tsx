c537d3237cc8e40ff73f2c166d0aa9d7b9905689
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-28 14:16:46 +0200

    commit

---
+++ src/assets/expense-tracker/components/expenseList.tsx
@@ -0,0 +1,21 @@
+
+
+const expenseList = () => {
+  return (
+    <table className="table table-bordered">
+        <thead>
+            <tr>
+                <th>Description</th>
+                <th>Amount</th>
+                <th>Category</th>
+                <th></th>
+            </tr>
+        </thead>
+  <tbody>
+
+  </tbody>
+    </table>
+  )
+}
+
+export default expenseList
