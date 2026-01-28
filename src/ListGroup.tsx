5c05853f680c04c433568187bd9f4b0706b91348
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-28 13:08:30 +0200

    commit

--- src/ListGroup.tsx
+++ src/ListGroup.tsx
@@ -1,10 +1,15 @@
 import styles from './ListGroup.module.css'
 
-
 function ListGroup() {
   return (
     <>
-    
+      <ul className={styles['list-group']}>
+        <li className={`${styles['list-group-item']} ${styles['container']}`}>An item</li>
+        <li className={`${styles['list-group-item']} ${styles['container']}`}>A second item</li>
+        <li className={`${styles['list-group-item']} ${styles['container']}`}>A third item</li>
+        <li className={`${styles['list-group-item']} ${styles['container']}`}>A fourth item</li>
+        <li className={`${styles['list-group-item']} ${styles['container']}`}>And a fifth one</li>
+      </ul>
     </>
   );
 }
