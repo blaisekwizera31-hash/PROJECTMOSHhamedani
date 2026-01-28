46709974994be78ec589ea4aeca39a4b3c0002f8
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-28 14:57:17 +0200

    commit

--- src/assets/expense-tracker/components/expenseList.tsx
+++ src/assets/expense-tracker/components/expenseList.tsx
@@ -1,37 +1,39 @@
-interface Expense{
-    id: number;
-    description: string;
-    amount: number;
-    category: string;
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
 }
 
-
-interface Props{
-   expenses: Expense[] 
+interface Props {
+  expenses: Expense[];
 }
 
-const expenseList = ({expenses}:Props) => {
+const expenseList = ({ expenses }: Props) => {
   return (
     <table className="table table-bordered">
-        <thead>
-            <tr>
-                <th>Description</th>
-                <th>Amount</th>
-                <th>Category</th>
-                <th></th>
-            </tr>
-        </thead>
-  <tbody>
-      {expenses.map(expense => 
-      <tr key={expense.id}>
-        <td>{expense.description}</td>
-        <td>{expense.amount}</td>
-        <td>{expense.category}</td>
-        <td><button className="btn btn-outline-danger">Delete</button></td>
-      </tr>)}
-  </tbody>
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+          <th>Category</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((expense) => (
+          <tr key={expense.id}>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.category}</td>
+            <td>
+              <button className="btn btn-outline-danger">Delete</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default expenseList
+export default expenseList;
